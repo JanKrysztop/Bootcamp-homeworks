9c29f4a018352f617396f056c5a8d71c5e46000d
Jan Krysztop <jankrysztop@gmail.com>
2022-01-07 16:23:31 +0100

    Dough balls display, shrink onClick and raw cookies counter added

--- homeworks/03-react-bakery/src/App.js
+++ homeworks/03-react-bakery/src/App.js
@@ -4,9 +4,10 @@ import { FormDoughBall } from "./components/FormDoughBall";
 const flourPerDoughBall = 10;
 
 export const App = () => {
-  const [doughBallsCounter, setDoughBallsCounter] = useState(0);
+  const [doughBalls, setDoughBalls] = useState([]);
   const [flourCounter, setFlourCounter] = useState(100);
   const [isFlourAtHand, setIsFlourAtHand] = useState(false);
+  const [rawCookieCounter, setRawCookieCounter] = useState(0);
 
   const notEnoughFlour = flourCounter < flourPerDoughBall;
 
@@ -19,20 +20,64 @@ export const App = () => {
   }, [notEnoughFlour]);
 
   const registerNewDoughBall = useCallback(() => {
-    setDoughBallsCounter((numberOfDoughBalls) => numberOfDoughBalls + 1);
-    setIsFlourAtHand(false)
+    setDoughBalls((doughBalls) => [
+      ...doughBalls,
+      { id: Date.now(), size: 50 },
+    ]);
+    setIsFlourAtHand(false);
   }, []);
 
   return (
     <div>
       <h1>React Bakery</h1>
       <p>Ilość mąki: {flourCounter}kg</p>
-      <p>Liczba ulepionych ciastowych kul: {doughBallsCounter}</p>
+      <p>Liczba ulepionych ciastowych kul: {doughBalls.length}</p>
       <FormDoughBall
         hasIngredients={isFlourAtHand}
         consumeIngredients={consumeFlour}
         onDoughBallReady={registerNewDoughBall}
       />
+      <div style={{display: "flex"}}>
+        {doughBalls.map((ball) => {
+          return (
+            <div
+              key={ball.id}
+              style={{
+                width: 50,
+                height: 50,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              onClick={() => {
+                setRawCookieCounter((value) => value + 1);
+                setDoughBalls((doughBalls) => 
+                doughBalls
+                  .map((doughBall) => 
+                    doughBall.id === ball.id
+                      ? {
+                        ...doughBall,
+                        size: doughBall.size - 5,
+                        }
+                      : doughBall
+                  )
+                  .filter((ball) => ball.size > 0)
+                );
+              }}
+            >
+              <div
+                style={{
+                  background: "yellow",
+                  width: ball.size,
+                  height: ball.size,
+                  borderRadius: "50%",
+                }}
+              />
+            </div> 
+          );
+        })}
+      </div>
+      <p>Liczba ulepionych ciastek: {rawCookieCounter}</p>
     </div>
   );
 };
