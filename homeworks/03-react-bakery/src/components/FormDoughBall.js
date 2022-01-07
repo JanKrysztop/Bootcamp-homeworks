c03004872919b7b69a7224de5ece1b2c039ce8bd
Jan Krysztop <jankrysztop@gmail.com>
2022-01-07 13:37:12 +0100

    Working flour counter and alert added

--- homeworks/03-react-bakery/src/components/FormDoughBall.js
+++ homeworks/03-react-bakery/src/components/FormDoughBall.js
@@ -5,10 +5,12 @@ export const FormDoughBall = (props) => {
   const [shouldFormDoughBalls, setShouldFormDoughBalls] = useState(false);
   const [progress, setProgress] = useState(0);
   const [doughBallsCounter, setDoughBallsCounter] = useState(0);
+  const [flourCounter, setFlourCounter] = useState(100);
 
+  const canFormDoughBall = flourCounter >= 10;
 
   useEffect(() => {
-    if(shouldFormDoughBalls === false) {
+    if (shouldFormDoughBalls === false) {
       return;
     }
 
@@ -18,28 +20,37 @@ export const FormDoughBall = (props) => {
           setDoughBallsCounter((counter) => counter + 1);
           return 0;
         }
+        if (oldProgress === 0) {
+          if (canFormDoughBall) {
+            setFlourCounter((flour) => flour - 10);
+          } else {
+            return oldProgress;
+          }
+        }
         return oldProgress + 0.25;
       });
-    }, 1000);
+    }, 100);
 
     return () => {
       clearInterval(id);
     };
-  }, [shouldFormDoughBalls]);
+  }, [shouldFormDoughBalls, canFormDoughBall]);
 
   const handleClick = () => {
-    setShouldFormDoughBalls((should) => !should)
+    setShouldFormDoughBalls((should) => !should);
   };
 
   return (
     <div>
+      <p>Ilość mąki: {flourCounter}kg</p>
       <p>Liczba ulepionych ciastowych kul: {doughBallsCounter}</p>
-      <ProgressBar progress={progress}/>
+      <ProgressBar progress={progress} />
       <button onClick={handleClick}>
-        {shouldFormDoughBalls === false 
-          ? "Ulep ciasto" 
-          : "Zatrzymaj lepienie"}
+        {shouldFormDoughBalls === false ? "Ulep ciasto" : "Zatrzymaj lepienie"}
       </button>
+      {shouldFormDoughBalls === true && canFormDoughBall === false && (
+        <span style={{ color: "red" }}>Za mało mąki</span>
+      )}
     </div>
   );
-}
+};
