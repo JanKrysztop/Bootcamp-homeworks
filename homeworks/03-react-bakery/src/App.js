e1ec5828b0e00116c3a4fa7c676fad3819bef8d5
Jan Krysztop <jankrysztop@gmail.com>
2022-01-07 15:56:09 +0100

    Structure changes

--- homeworks/03-react-bakery/src/App.js
+++ homeworks/03-react-bakery/src/App.js
@@ -1,13 +1,38 @@
-import React from "react";
-import { ProgressBar } from "./components/ProgressBar";
-import { FormDoughBall } from "./components/FormDoughBall"
+import { useState, useCallback } from "react";
+import { FormDoughBall } from "./components/FormDoughBall";
+
+const flourPerDoughBall = 10;
 
 export const App = () => {
+  const [doughBallsCounter, setDoughBallsCounter] = useState(0);
+  const [flourCounter, setFlourCounter] = useState(100);
+  const [isFlourAtHand, setIsFlourAtHand] = useState(false);
+
+  const notEnoughFlour = flourCounter < flourPerDoughBall;
+
+  const consumeFlour = useCallback(() => {
+    if (notEnoughFlour) {
+      return;
+    }
+    setFlourCounter((flour) => flour - flourPerDoughBall);
+    setIsFlourAtHand(true);
+  }, [notEnoughFlour]);
+
+  const registerNewDoughBall = useCallback(() => {
+    setDoughBallsCounter((numberOfDoughBalls) => numberOfDoughBalls + 1);
+    setIsFlourAtHand(false)
+  }, []);
+
   return (
     <div>
       <h1>React Bakery</h1>
-
-      <FormDoughBall />
+      <p>Ilość mąki: {flourCounter}kg</p>
+      <p>Liczba ulepionych ciastowych kul: {doughBallsCounter}</p>
+      <FormDoughBall
+        hasIngredients={isFlourAtHand}
+        consumeIngredients={consumeFlour}
+        onDoughBallReady={registerNewDoughBall}
+      />
     </div>
   );
-}
+};
--- homeworks/03-react-bakery/src/components/FormDoughBall.js
+++ homeworks/03-react-bakery/src/components/FormDoughBall.js
@@ -1,40 +1,42 @@
 import { useState, useEffect } from "react";
 import { ProgressBar } from "./ProgressBar";
 
-export const FormDoughBall = (props) => {
+
+
+export const FormDoughBall = ({
+  hasIngredients,
+  consumeIngredients,
+  onDoughBallReady,
+}) => {
   const [shouldFormDoughBalls, setShouldFormDoughBalls] = useState(false);
   const [progress, setProgress] = useState(0);
-  const [doughBallsCounter, setDoughBallsCounter] = useState(0);
-  const [flourCounter, setFlourCounter] = useState(100);
-
-  const canFormDoughBall = flourCounter >= 10;
+  
+  useEffect(() => {
+    if (progress === 1) {
+      onDoughBallReady();
+      setProgress(0);
+    }
+  }, [progress, onDoughBallReady]);
 
   useEffect(() => {
-    if (shouldFormDoughBalls === false) {
+    if (shouldFormDoughBalls && !hasIngredients) {
+      consumeIngredients();
+    }
+  }, [consumeIngredients, shouldFormDoughBalls, hasIngredients]);
+
+  useEffect(() => {
+    if (!shouldFormDoughBalls || !hasIngredients) {
       return;
     }
 
     const id = setInterval(() => {
-      setProgress((oldProgress) => {
-        if (oldProgress === 1) {
-          setDoughBallsCounter((counter) => counter + 1);
-          return 0;
-        }
-        if (oldProgress === 0) {
-          if (canFormDoughBall) {
-            setFlourCounter((flour) => flour - 10);
-          } else {
-            return oldProgress;
-          }
-        }
-        return oldProgress + 0.25;
-      });
+      setProgress((oldProgress) => Math.min(oldProgress + 0.25, 1));
     }, 100);
 
     return () => {
       clearInterval(id);
     };
-  }, [shouldFormDoughBalls, canFormDoughBall]);
+  }, [shouldFormDoughBalls, hasIngredients]);
 
   const handleClick = () => {
     setShouldFormDoughBalls((should) => !should);
@@ -42,13 +44,11 @@ export const FormDoughBall = (props) => {
 
   return (
     <div>
-      <p>Ilość mąki: {flourCounter}kg</p>
-      <p>Liczba ulepionych ciastowych kul: {doughBallsCounter}</p>
       <ProgressBar progress={progress} />
       <button onClick={handleClick}>
         {shouldFormDoughBalls === false ? "Ulep ciasto" : "Zatrzymaj lepienie"}
       </button>
-      {shouldFormDoughBalls === true && canFormDoughBall === false && (
+      {shouldFormDoughBalls && !hasIngredients && (
         <span style={{ color: "red" }}>Za mało mąki</span>
       )}
     </div>
