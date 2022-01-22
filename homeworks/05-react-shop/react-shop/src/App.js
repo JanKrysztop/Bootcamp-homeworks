5c41dd449e34eab47a1f76fb1c0990a4be0b2b90
Jan Krysztop <jankrysztop@gmail.com>
2022-01-22 18:27:16 +0100

    Products list rendered

--- homeworks/05-react-shop/react-shop/src/App.js
+++ homeworks/05-react-shop/react-shop/src/App.js
@@ -1,9 +1,29 @@
-
+import { useState, useEffect } from "react";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/products.json')
+      .then(res => res.json())
+      .then(data => setProducts(data))
+  }, [])
+
+
+  
   return (
     <div >
       <h1>React Emoji Shop ≧◡≦</h1>
+      <div>
+        <h2>Products:</h2>
+          <ul>
+            {products.map((product) => {
+              return (
+                <li key={product.id}>{product.name}</li>
+              );
+              })}
+          </ul>
+      </div>
     </div>
   );
 }
