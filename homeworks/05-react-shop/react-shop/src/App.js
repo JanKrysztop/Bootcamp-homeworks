8e434cec1b456fc5d807b737702aa74fa6096073
Jan Krysztop <jankrysztop@gmail.com>
2022-01-25 11:39:07 +0100

    Buttons and info display added

--- homeworks/05-react-shop/react-shop/src/App.js
+++ homeworks/05-react-shop/react-shop/src/App.js
@@ -1,28 +1,54 @@
 import { useState, useEffect } from "react";
+import style from './style.module.css'
 
+const Info = ({ product }) => {
+  return (
+    <div key={product.id} className={style.info}>
+      <p>{product.name}</p>
+      <p>
+        {`${product.price.value}` / 100 + " "}
+        {product.price.currency}
+      </p>
+      <p className={style.emot}>{product.item}</p>
+    </div>
+  );
+};
 function App() {
   const [products, setProducts] = useState([]);
+  const [infoDisplay, setInfoDisplay] = useState(null);
 
+  const selectedProduct = products.find(
+    (product) => product.id === infoDisplay
+  );
   useEffect(() => {
-    fetch('http://localhost:3000/products.json')
-      .then(res => res.json())
-      .then(data => setProducts(data))
-  }, [])
+    fetch("http://localhost:3000/products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
 
-
-  
   return (
-    <div >
+    <div className={style.wrapper}>
       <h1>React Emoji Shop ≧◡≦</h1>
-      <div>
-        <h2>Products:</h2>
-          <ul>
-            {products.map((product) => {
-              return (
-                <li key={product.id}>{product.name}</li>
-              );
-              })}
-          </ul>
+      <h2>Products:</h2>
+      <div className={style.products}>
+        <ul>
+          {products.map((product) => {
+            return (
+              <div key={product.id}>
+                <li>
+                  <p>{product.name}</p>
+                  <button
+                    type="button"
+                    onClick={() => setInfoDisplay(product.id)}
+                  >
+                    Show Details
+                  </button>
+                </li>
+              </div>
+            );
+          })}
+        </ul>
+        {infoDisplay ? <Info product={selectedProduct} /> : null}
       </div>
     </div>
   );
